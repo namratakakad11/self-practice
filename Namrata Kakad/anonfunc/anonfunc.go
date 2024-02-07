3e8fef0bd2d8af57fc8b75960812af7b8531b61d
Namrata kakad <134642828+namratakakad11@users.noreply.github.com>
2024-02-07 10:08:35 +0530

    Update anonfunc.go

--- Namrata Kakad/anonfunc/anonfunc.go
+++ Namrata Kakad/anonfunc/anonfunc.go
@@ -7,6 +7,7 @@ func main(){
 	fmt.Println("executing")
 	arr := [] int{1,2,3}
 	ch := make(chan int, len(arr))
+	// annon func with go routine
 	go func(arr []int, ch chan int){
 		for _,ele :=range arr{
 			ch <- ele +9
